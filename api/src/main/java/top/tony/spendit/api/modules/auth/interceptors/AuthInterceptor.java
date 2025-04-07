475ff2e4d39e22f959c4d713c3c6a108fee4a5d2
Lord Turmoil <swift-phantom@outlook.com>
2025-04-07 21:51:47 +0800

    🐛fix CORS by always allowing preflight OPTIONS requests

--- api/src/main/java/top/tony/spendit/api/modules/auth/interceptors/AuthInterceptor.java
+++ api/src/main/java/top/tony/spendit/api/modules/auth/interceptors/AuthInterceptor.java
@@ -30,6 +30,11 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // Always allow preflight requests.
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
         String jwt = jwtCookieSetter.get(request);
         AuthPayload payload;
 
